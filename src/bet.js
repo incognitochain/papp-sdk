af2aa648cbbd2cbfc8a9ba322261235de2b412b9
Keresh <vuong@autonomous.nyc>
2019-12-04 20:25:47 +0700

    Update bet

--- src/bet.js
+++ src/bet.js
@@ -5,19 +5,20 @@ const BET_RATE = 3;
 
 // this game is yours!
 async function bet({ betAmount, paymentAddress, betDiceNumber }) {
-  const isWin = Math.round(Math.random() * 6) === betDiceNumber;
-  return isWin ? await handleWin({ betAmount, paymentAddress, betDiceNumber }) : handleLose({ betAmount, paymentAddress, betDiceNumber });
+  const betDiceNumberResult = Math.round(Math.random() * 5) + 1; // [1..6]
+  const isWin = betDiceNumberResult === betDiceNumber;
+  return isWin ? await handleWin({ betAmount, paymentAddress, betDiceNumber, betDiceNumberResult }) : handleLose({ betAmount, paymentAddress, betDiceNumber, betDiceNumberResult });
 }
 
 // user lose
-function handleLose({ betAmount, paymentAddress, betDiceNumber }) {
-  console.debug(`${paymentAddress} losed ${betAmount}, bet ${betDiceNumber}`);
+function handleLose({ betAmount, paymentAddress, betDiceNumber, betDiceNumberResult }) {
+  console.debug(`${paymentAddress} losed ${betAmount}, bet: ${betDiceNumber}, result: ${betDiceNumberResult}`);
 
-  return { lose: betAmount } ;
+  return { lose: betAmount, betDiceNumberResult } ;
 }
 
 // user win
-async function handleWin({ betAmount, paymentAddress, betDiceNumber }) {
+async function handleWin({ betAmount, paymentAddress, betDiceNumber, betDiceNumberResult }) {
 
   const winAmount = betAmount * BET_RATE;
 
@@ -27,10 +28,10 @@ async function handleWin({ betAmount, paymentAddress, betDiceNumber }) {
     amount: winAmount
   });
 
-  console.debug(`${paymentAddress} won ${winAmount}, bet ${betDiceNumber}. TxID: ${tx.txId}`);
+  console.debug(`${paymentAddress} won ${winAmount}, bet: ${betDiceNumber}, result: ${betDiceNumberResult}. TxID: ${tx.txId}`);
 
 
-  return { win: winAmount, message: `You will receive ${winAmount} nano PRV in a couple minutes`, txId: tx.txId } ;
+  return { win: winAmount, betDiceNumberResult, message: `You will receive ${winAmount} nano PRV in a couple minutes`, txId: tx.txId } ;
 }
 
 async function handleBetRequest(req, res) {
